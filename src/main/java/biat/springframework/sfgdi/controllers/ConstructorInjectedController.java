ccbed48b075a15b873656f6c58b11a7f6c233fb4
aymenbraiek <aymen.braiek@esprit.tn>
2021-04-12 18:03:02 +0100

    adding DI ConstructorInjectedController -closes #5

---
+++ src/main/java/biat/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -0,0 +1,15 @@
+package biat.springframework.sfgdi.controllers;
+
+import biat.springframework.sfgdi.services.GreetingService;
+
+public class ConstructorInjectedController {
+    private final GreetingService greetingService;
+
+    public ConstructorInjectedController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    public String getGreeting(){
+        return greetingService.sayGreeting();
+    }
+}
